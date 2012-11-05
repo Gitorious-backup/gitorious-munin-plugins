b63ea20b4e44c67bbd22e7b14a41a0ecce9035d5
Marius Mathiesen <marius@shortcut.no>
2012-11-05 14:14:57 +0100

    Add gitorious clones plugin

---
+++ lib/gitorious-munin-plugins/plugins/gitorious_clones.rb
@@ -0,0 +1,20 @@
+# SSH keys
+database =  GitoriousMuninPlugins::Database.new
+cmd = ARGV.shift
+
+case cmd
+when "autoconf"
+  puts "no"
+when "config"
+  puts "graph_title git:// Clones (within last 24 hrs)"
+  puts 'graph_args -l 0'
+  puts 'graph_vlabel Cloners'
+  puts 'graph_category Gitorious'
+  puts "new.label Clones"
+  puts "new.draw LINE2"
+  puts "new.type GAUGE"
+else
+  select("select count(*) as count from cloners where date >= date_sub(now(), interval 24 hour)").each_hash do |row|
+    puts "new.value #{row['count'].to_i}"
+  end
+end
