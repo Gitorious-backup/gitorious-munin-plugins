4bf848a5e77d005268b4237ba21fd7a70cef4da1
Marius Mathiesen <marius@shortcut.no>
2012-11-06 13:45:38 +0100

    Add repositories plugin

--- lib/gitorious-munin-plugins/plugins/gitorious_repositories.rb
+++ lib/gitorious-munin-plugins/plugins/gitorious_repositories.rb
@@ -4,15 +4,15 @@ case cmd
   when "autoconf"
     puts "no"
   when "config"
-    puts "graph_title New Projects (within last 24 hrs)"
+    puts "graph_title New Repositories (within last 24 hrs)"
     puts 'graph_args -l 0'
-    puts 'graph_vlabel Projects'
+    puts 'graph_vlabel Repositories'
     puts 'graph_category Gitorious'
-    puts "new.label Projects"
+    puts "new.label Repositories"
     puts "new.draw LINE2"
     puts "new.type GAUGE"
   else
-    database.select("select count(*) as count from projects where created_at >= date_sub(now(), interval 24 hour)").each_hash do |row|
+    database.select("select count(*) as count from repositories where created_at >= date_sub(now(), interval 24 hour)").each_hash do |row|
       puts "new.value #{row['count'].to_i}"
     end
 end
